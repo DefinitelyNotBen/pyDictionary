5dc741783726f7a008b05717b845cd85d16aa850
DefinitelyNotBen <b.irving94@protonmail.com>
2021-02-14 17:07:36 +0000

    add for loop to close matches clause

--- main.py
+++ main.py
@@ -9,8 +9,17 @@ def check(s):
     if s in data:
         return data[s]
 
+    # Check if there are any close matches to the given response
     elif len(get_close_matches(s, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(s, data.keys())[0]
+        # if there are then loop over values, checking with user if that is the one they wanted
+        for key in get_close_matches(s, data.keys()):
+            check = input(f"Did you mean {key} instead? Y/N ")
+
+            if check.lower() == "y" :
+                return data[key]
+
+        # if nothing is found then return not found 
+        return "Word not found, please check input"
     else:
         return "Word not found, please check input"
 
